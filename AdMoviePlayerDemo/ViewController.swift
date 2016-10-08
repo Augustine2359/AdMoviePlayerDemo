6c3be2db745098cc34e0a8866ef790859fb88837
Augustine Cheong <augustine_cheong@hotmail.com>
2016-10-09 05:01:39 +0900

    Add webview to VC

--- AdMoviePlayerDemo/ViewController.swift
+++ AdMoviePlayerDemo/ViewController.swift
@@ -7,11 +7,26 @@
 //
 
 import UIKit
+import WebKit
 
 class ViewController: UIViewController {
+    var webView: WKWebView
 
+    required init?(coder aDecoder: NSCoder) {
+        webView = WKWebView()
+        webView.translatesAutoresizingMaskIntoConstraints = false
+
+        super.init(coder: aDecoder)
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        view.backgroundColor = UIColor.red
+        view.addSubview(webView)
+
+        addConstraintsToWebView()
+        
         // Do any additional setup after loading the view, typically from a nib.
     }
 
@@ -19,7 +34,17 @@ class ViewController: UIViewController {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
-
-
+    
+    func addConstraintsToWebView() {
+        let leftConstraint = NSLayoutConstraint(item: view, attribute: .trailing, relatedBy: .equal, toItem: webView, attribute: .trailing, multiplier: 1, constant: 0)
+        let rightConstraint = NSLayoutConstraint(item: view, attribute: .leading, relatedBy: .equal, toItem: webView, attribute: .leading, multiplier: 1, constant: 0)
+        let topConstraint = NSLayoutConstraint(item: topLayoutGuide, attribute: .bottom, relatedBy: .equal, toItem: webView, attribute: .top, multiplier: 1, constant: 0)
+        let heightConstraint = NSLayoutConstraint(item: view, attribute: .height, relatedBy: .equal, toItem: webView, attribute: .height, multiplier: 2, constant: 0)
+        
+        view.addConstraint(leftConstraint)
+        view.addConstraint(rightConstraint)
+        view.addConstraint(heightConstraint)
+        view.addConstraint(topConstraint)
+    }
 }
 
