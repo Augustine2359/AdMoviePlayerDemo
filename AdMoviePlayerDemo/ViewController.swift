19c2f55eb9c918b0738021784753f7bc893f8df7
Augustine Cheong <augustine_cheong@hotmail.com>
2016-10-09 07:19:02 +0900

    move more functions to the class

--- AdMoviePlayerDemo/ViewController.swift
+++ AdMoviePlayerDemo/ViewController.swift
@@ -10,42 +10,17 @@ import UIKit
 import WebKit
 
 class ViewController: UIViewController {
-    var advertisementView: AdvertisementView
-
-    required init?(coder aDecoder: NSCoder) {
-        advertisementView = AdvertisementView(videoURL: "http://hitokuse.com/videos/top-video.mp4")
-        
-        super.init(coder: aDecoder)
-    }
+    @IBOutlet var advertisementView: AdvertisementView!
 
     override func viewDidLoad() {
         super.viewDidLoad()
 
         view.backgroundColor = UIColor.red
-        view.addSubview(advertisementView)
-        addConstraintsToadvertisementView()
+        advertisementView.playAdvertisement(urlString: "http://hitokuse.com/videos/top-video.mp4")
     }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
-    }
-    
-    func addConstraintsToadvertisementView() {
-        let leftConstraint = NSLayoutConstraint(item: view, attribute: .trailing, relatedBy: .equal, toItem: advertisementView, attribute: .trailing, multiplier: 1, constant: 0)
-        let rightConstraint = NSLayoutConstraint(item: view, attribute: .leading, relatedBy: .equal, toItem: advertisementView, attribute: .leading, multiplier: 1, constant: 0)
-        let topConstraint = NSLayoutConstraint(item: topLayoutGuide, attribute: .bottom, relatedBy: .equal, toItem: advertisementView, attribute: .top, multiplier: 1, constant: 0)
-        let heightConstraint = NSLayoutConstraint(item: view, attribute: .height, relatedBy: .equal, toItem: advertisementView, attribute: .height, multiplier: 2, constant: 0)
-        
-        view.addConstraint(leftConstraint)
-        view.addConstraint(rightConstraint)
-        view.addConstraint(heightConstraint)
-        view.addConstraint(topConstraint)
-    }
-    
-    override func viewDidAppear(_ animated: Bool) {
-        super.viewDidAppear(animated)
-        
-        advertisementView.advertisementPlay()
     }
 }
--- AdvertisementView.swift
+++ AdvertisementView.swift
@@ -13,32 +13,18 @@ import WebKit
 class AdvertisementView: UIView {
     var webView: WKWebView?
     var button: UIButton
-    var videoURLString: String
-        
+
     required init?(coder aDecoder: NSCoder) {
         button = UIButton()
-        videoURLString = "http://hitokuse.com/videos/top-video.mp4"
         super.init(coder: aDecoder)
-        translatesAutoresizingMaskIntoConstraints = false
-        addConstraintsToWebView()
-        addConfigurationToWebView()
-    }
 
-    init(videoURL: String) {
-        button = UIButton()
-        videoURLString = videoURL
-        super.init(frame: .zero)
-        
         translatesAutoresizingMaskIntoConstraints = false
-        
         addConfigurationToWebView()
         webView!.translatesAutoresizingMaskIntoConstraints = false
         addSubview(webView!)
         addConstraintsToWebView()
-        
-        advertisementPlay()
     }
-    
+
     func addConstraintsToWebView() {
         let leftConstraint = NSLayoutConstraint(item: self, attribute: .trailing, relatedBy: .equal, toItem: webView, attribute: .trailing, multiplier: 1, constant: 0)
         let rightConstraint = NSLayoutConstraint(item: self, attribute: .leading, relatedBy: .equal, toItem: webView, attribute: .leading, multiplier: 1, constant: 0)
@@ -73,9 +59,10 @@ class AdvertisementView: UIView {
         webView = WKWebView(frame: .zero, configuration: configuration)
     }
     
-    func advertisementPlay() {
-        let url = URL(string: videoURLString)!
-        let urlRequest = URLRequest(url: url)
-        webView!.load(urlRequest)
+    func playAdvertisement(urlString:String) {
+        if let url = URL(string: urlString) {
+            let urlRequest = URLRequest(url: url)
+            webView!.load(urlRequest)
+        }
     }
 }
