3c7695700a318c031afc8240ecfeb82f4e11d4db
Augustine Cheong <augustine_cheong@hotmail.com>
2016-10-09 09:11:37 +0900

    Added support for programmatically adding cell

--- AdMoviePlayerDemo/AdvertisementTableViewCell.swift
+++ AdMoviePlayerDemo/AdvertisementTableViewCell.swift
@@ -24,9 +24,27 @@ class AdvertisementTableViewCell: UITableViewCell {
         super.init(coder: aDecoder)
     }
     
+    override init(style: UITableViewCellStyle, reuseIdentifier: String?) {
+        advertisementView = AdvertisementView()
+        advertisementView.translatesAutoresizingMaskIntoConstraints = false
+        super.init(style: style, reuseIdentifier: reuseIdentifier)
+        contentView.addSubview(advertisementView)
+        addConstraintsFor(subview: advertisementView)
+    }
+    
     func playAdvertisement(urlString:String) {
         advertisementView.playAdvertisement(urlString: urlString)
     }
     
-    
+    func addConstraintsFor(subview: UIView) {
+        let leftConstraint = NSLayoutConstraint(item: contentView, attribute: .trailing, relatedBy: .equal, toItem: subview, attribute: .trailing, multiplier: 1, constant: 0)
+        let rightConstraint = NSLayoutConstraint(item: contentView, attribute: .leading, relatedBy: .equal, toItem: subview, attribute: .leading, multiplier: 1, constant: 0)
+        let topConstraint = NSLayoutConstraint(item: contentView, attribute: .top, relatedBy: .equal, toItem: subview, attribute: .top, multiplier: 1, constant: 0)
+        let heightConstraint = NSLayoutConstraint(item: contentView, attribute: .height, relatedBy: .equal, toItem: subview, attribute: .height, multiplier: 1, constant: 0)
+        
+        addConstraint(leftConstraint)
+        addConstraint(rightConstraint)
+        addConstraint(heightConstraint)
+        addConstraint(topConstraint)
+    }
 }
